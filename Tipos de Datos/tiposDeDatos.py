c1b7afb945928036f88028c2871f164bbe96325e
Felipe Ponce <felipeponce2005@gmail.com>
2026-08-12 12:25:44 -0400

    Archivo explicativo de los tipos de datos basicos

---
+++ Tipos de Datos/tiposDeDatos.py
@@ -0,0 +1,21 @@
+#Los datos tipo String se pueden escribir con comillas simples:
+# 'Hola Mundo!'
+# o tambien se pueden interpretar con comillas dobles
+# "Hola Mundo"
+
+print("Este dato es de tipo String") 
+
+#Para los numeros depende tambien como se escriban
+#para interpretar un numero entero este debe de escribirse tal cual como un entero:
+# 40
+#Para un valor decimal o flotante este debe estar escrito seguido de un punto:
+# 40.5
+
+print("Este dato es entero: ", 40)
+print("Este dato es de tipo flotante: ", 40.5)
+
+#Para los valores booleans deben escribirse con la primera letra en mayuscula
+# True
+# False
+print(True)
+print(False)
