92d2c50942e2205567b22c51834eb9e1c6244bcf
Felipe Ponce <felipeponce2005@gmail.com>
2026-08-12 15:05:00 -0400

    como utilizar los varios tipos de comparadores

---
+++ operadores/op_comparativos.py
@@ -0,0 +1,20 @@
+#comparador ==
+
+esIgualQue = 5 == 6
+
+esDistintoDe = 5 != 6
+
+mayorQue = 5 > 6
+
+menorQue = 5 < 6
+
+mayorIgual = 5 >= 6
+
+menorIgual = 5 <= 6
+
+print(f"es igual que ==: {esIgualQue}\n"
+      f"es distinto que !=: {esDistintoDe}\n"
+      f"mayor que >: {mayorQue}\n"
+      f"menor que <: {menorQue}\n"
+      f"mayor o igual que >=: {mayorIgual}\n"
+      f"menor o igual que <=: {menorIgual}")
