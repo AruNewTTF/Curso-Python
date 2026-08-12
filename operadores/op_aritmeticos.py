318ee3292793ecb63897dc43b825dfe4fa3c5ee8
Felipe Ponce <felipeponce2005@gmail.com>
2026-08-12 13:22:03 -0400

    operadores matematicos o aritmeticos en python

---
+++ operadores/op_aritmeticos.py
@@ -0,0 +1,33 @@
+#lo basico, suma y resta +, -
+
+suma = 12 + 5
+resta = 10 -5
+
+#multiplicaciones y divisiones
+
+multi = 15 * 5
+division = 50 / 5 #por usar / devolverá un valor float
+
+#potenciacion (exponente) **
+
+exponente = 10 ** 5
+
+#division baja //, devuelve enteros redondeados hacia abajo
+
+div_baja = 10 // 5
+
+#resto o modulo %, muestra unicamente el resto de una division
+
+resto = 10 % 5
+
+print(f"suma: {suma}\n"
+      f"resta: {resta}\n"
+      f"multiplicacion: {multi}\n"
+      f"division: {division}\n"
+      f"potenciacion: {exponente}\n"
+      f"division baja: {div_baja}\n"
+      f"resto: {resto}")
+
+#type() para saber que tipo de dato es
+
+print(type(div_baja))
