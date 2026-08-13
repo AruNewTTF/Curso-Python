9960b4d0e9e95be7c14b5e1effcbdb4d925f8250
Felipe Ponce <felipeponce2005@gmail.com>
2026-08-13 17:25:45 -0400

    nueva carpeta mostrando usos sobre los inputs

---
+++ inputs/input-nombres.py
@@ -0,0 +1,7 @@
+#perdirle un dato al user
+#el valor que devuelve input SIEMPRE es de cadena String
+nombre = input("Ingresa tu nombre: ")
+
+#Hay que mostrar el dato ingresado
+print(f"Bienvendio {nombre}")
+
---
+++ inputs/input-numeros.py
@@ -0,0 +1,12 @@
+#pedirle un numero al user
+numero = input("ingrese su numero: ")
+
+#multiplicando el numero por 2
+#la funcion int() permite que el valor de input pase 
+#a ser un numero entero para que tenga sentido
+
+resultado = int(numero) * 2
+
+#mostrando resultados
+
+print(f"El resultado de la duplicacion es: {resultado}")
