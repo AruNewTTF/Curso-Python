515f8c387ec86c408abfa0edf1f3f789c3c8c1ae
Felipe Ponce <felipeponce2005@gmail.com>
2026-08-18 15:07:24 -0400

    Archivo donde se trabaja con bucle while

---
+++ bucles/bucle_while.py
@@ -0,0 +1,13 @@
+
+contador = 0
+
+#While funcionará mientras que la condicion sea True
+while contador < 10:
+    #Si dejamos el print sin nada más
+    #este bucle será infinito, ya que nada cambiará el valor de
+    #contador para que este sea False
+    print(contador)
+    contador += 1
+print("Bucle while a finalizado")
+
+print("\n-------------------\n")
