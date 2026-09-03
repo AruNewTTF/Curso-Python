cbcb5708bbdb5ad2e8d2a2dcc5d2d1b6d1f2e24b
Felipe <felipeponce2005@gmail.com>
2026-09-03 13:12:32 -0400

    modulo con funciones matematicas basicas entre 2 valores

---
+++ modulos/modulo_matematico.py
@@ -0,0 +1,27 @@
+#suma
+def sumar():
+    a = int(input("Ingrese el primer numero a sumar: ")) 
+    b = int(input("Ingrese el segundo numero a sumar: "))
+    resultado = a + b
+    return resultado
+
+#resta
+def resta():
+    a = int(input("Ingrese el primer numero a restar: ")) 
+    b = int(input("Ingrese el segundo numero a restar: "))
+    resultado = a - b
+    return resultado
+
+#multiplicacion
+def multiplicar():
+    a = int(input("Ingrese el primer numero a multiplicar: ")) 
+    b = int(input("Ingrese el segundo numero a multiplicar: "))
+    resultado = a * b
+    return resultado
+
+#division
+def division():
+    a = int(input("Ingrese el primer numero a dividir: ")) 
+    b = int(input("Ingrese el segundo numero a dividir: "))
+    resultado = a / b
+    return resultado
