a7f9a3d7796a835af6491429287ca1989c0cafc6
Felipe <felipeponce2005@gmail.com>
2026-09-03 12:52:29 -0400

    agregando un menu a los operadores

--- practico 2/operadores_matematicos.py
+++ practico 2/operadores_matematicos.py
@@ -30,4 +30,23 @@ def division():
     resultado = a / b
     return resultado
 
-print(f"El resultado es: {division()}")
+seleccionador = -1
+while seleccionador != 0:
+    print("\nElige con que operador quieres jugar:\n1. Suma\n2. Resta\n3. Multiplicacion \n4. Division \n0. Salir")
+    seleccionador = int(input("Ingrese un numero del menu: "))
+    if seleccionador == 1:
+        a = sumar()
+        print(f"El resultado de tu suma es: {a}")
+    elif seleccionador == 2:
+        a = resta()
+        print(f"El resultado de tu suma es: {a}")
+    elif seleccionador == 3:
+        a = multiplicar()
+        print(f"El resultado de tu mutiplicacion es: {a}")
+    elif seleccionador == 2:
+        a = division()
+        print(f"El resultado de tu divison es: {a}")
+    elif seleccionador == 0:
+        break
+    else:
+        print("\n--/--/--/--/--/--/--/--/\nDebes de seleccionar un numero del menu\n--/--/--/--/--/--/--/--/\n")
